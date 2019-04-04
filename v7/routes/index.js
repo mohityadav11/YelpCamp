69133b5772fe76343ab002c69dab2a0ba5526112
Mohit Yadav <mohit111198@gmail.com>
2019-04-04 12:35:53 +0530

    Created routes folder and clean app.js

---
+++ v7/routes/index.js
@@ -0,0 +1,45 @@
+app.get("/",function(req, res){
+	res.render("landing");
+});
+
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register",function(req, res){
+	var newUser = new User({username : req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			 res.redirect("/campgrounds");
+		});
+	});	
+});
+
+app.get("/login", function(req, res){
+	res.render("login");
+});
+
+app.post("/login",passport.authenticate("local", 
+	{
+	successRedirect : "/campgrounds", 
+	failureRedirect : "/login"
+	}),function(req, res){
+
+});
+
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+function isLoggedin(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
