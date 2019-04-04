9fa664876bb4266dc3c5af364283fa3c51a2bea1
Mohit Yadav <mohit111198@gmail.com>
2019-04-04 10:59:15 +0530

    modified app.js

--- v6/app.js
+++ v6/app.js
@@ -11,13 +11,22 @@ var User  = require("./models/user");
 var seedDB = require('./seeds')
 
 seedDB();
-
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v5",{useNewUrlParser : true});
-
-
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v6",{useNewUrlParser : true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+app.use(require('express-session')({
+	secret : "Its a Secret!",
+	resave : false,
+	saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/",function(req, res){
 	res.render("landing");
@@ -103,6 +112,35 @@ app.post("/campgrounds/:id/comments",function(req,res){
 		}
 	});
 });
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register",function(req, res){
+	var newUser = new User({username : req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			 res.redirect("/campgrounds");
+		});
+	});	
+});
+
+app.get("/login", function(req, res){
+	res.render("login");
+});
+
+app.post("/login",passport.authenticate("local", 
+	{
+	successRedirect : "/campgrounds", 
+	failureRedirect : "/login"
+	}),function(req, res){
+
+})
 
 app.listen(3000, 'localhost',function(){
 	console.log("Server has Started on PORT 3000");
