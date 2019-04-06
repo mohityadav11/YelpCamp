c567984ff24de2c450913d839aba859e97c76bc8
Mohit Yadav <mohit111198@gmail.com>
2019-04-06 12:09:29 +0530

    User association with comment

--- v8/app.js
+++ v8/app.js
@@ -14,7 +14,7 @@ var commentRoutes = require('./routes/comments'),
 	campgroundRoutes = require('./routes/campgrounds'),
 	indexRoutes = require('./routes/index');
 
-seedDB();
+// seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp_v6",{useNewUrlParser : true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
