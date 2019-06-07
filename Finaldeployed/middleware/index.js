50c0d8d5bf5823b9d7e50bbb2d9562c95fa71fc0
Mohit Yadav <mohit111198@gmail.com>
2019-06-07 14:35:10 +0530

    For deploying in heroku

---
+++ Finaldeployed/middleware/index.js
@@ -0,0 +1,57 @@
+
+var campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+	if(req.isAuthenticated()){
+		campground.findById(req.params.id, function(err, foundcampground){
+				if(err){
+					res.redirect("back");
+				}
+				else {
+						if(foundcampground.author.id.equals(req.user.id)){
+							next();
+						}
+						else {
+							res.redirect("back");
+						}		
+				}
+			});
+	} else {
+		req.flash("error","You need to be logged in to do that");
+		res.redirect("back");
+	}
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+				if(err){
+					res.redirect("back");
+				}
+				else {
+						if(foundComment.author.id.equals(req.user.id)){
+							next();
+						}
+						else {
+							res.redirect("back");
+						}		
+				}
+			});
+	} else {
+		req.flash("error","You need to be logged in to do that");
+		res.redirect("back");
+	}
+}
+
+middlewareObj.isLoggedin = function(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	req.flash("error","You need to be logged in to do that!");
+	res.redirect("/login");
+}
+
+module.exports = middlewareObj;
---
+++ Finaldeployed/routes/campgrounds.js
@@ -0,0 +1,82 @@
+
+var express = require('express');
+var router = express.Router();
+var campground = require('../models/campground');
+var middleware = require('../middleware');
+
+router.get("/", function(req, res){
+	campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.render("campgrounds/index",{campgrounds:allCampgrounds, currentUser : req.user});
+		}
+	});
+});
+
+router.post("/",middleware.isLoggedin,function(req, res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var desc = req.body.description;
+	var author = {
+		id : req.user._id,
+		username : req.user.username
+	}
+	var newCampground = {name:name, image:image, description:desc, author : author}
+	campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+router.get("/new",middleware.isLoggedin,function(req, res){
+	res.render("campgrounds/new");
+});
+
+router.get("/:id", function(req, res){
+	campground.findById(req.params.id).populate("comments").exec(function(err, foundcampground){
+		if(err){
+			console.log(err);
+		}
+		else {
+			console.log(foundcampground);
+			res.render("campgrounds/show", {campground:foundcampground});
+		}
+	});
+});
+
+router.get("/:id/edit",middleware.checkCampgroundOwnership, function(req,res){
+		campground.findById(req.params.id, function(err, foundcampground){
+		res.render("campgrounds/edit", {campground : foundcampground});	
+		});
+});
+
+router.put("/:id",middleware.checkCampgroundOwnership,function(req, res){
+	campground.findByIdAndUpdate(req.params.id , req.body.campground, function(err, updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		}
+		else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+router.delete("/:id",middleware.checkCampgroundOwnership,function(req, res){
+	campground.findByIdAndRemove(req.params.id , function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		}
+		else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+module.exports = router;
